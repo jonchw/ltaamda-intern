2b332d21d72654032e0f7b9299619e92c2512bf1
jonchw <90896455+jonchw@users.noreply.github.com>
2021-10-02 23:28:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const config = {
 	prefix: '/',
 	port: 443,
 	isSecure: true,
-    webIntegrationId: '4caLPkf1ugaOmVvjfo9SAr2wanQOM3SE'
+   	webIntegrationId: '4caLPkf1ugaOmVvjfo9SAr2wanQOM3SE'
 };
 require.config( {
-	baseUrl: 'https://${config.host}/resources', 
+	baseUrl: ( config.isSecure ? "https://" : "http://") + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources", 
     webIntegrationId: config.webIntegrationId
 } );
 
-require(['js/qlik'], (qlik) => {
-    qlik.on('error', (error) => console.error(error));
+require(['js/qlik'], function (qlik) {
+    qlik.on("error", function (error) {
+	    $( '#popupText' ).append( error.message + "<br>" );
+	    $( '#popup' ).fadeIn( 1000 );
   });
+	$( "#closePopup" ).click( function () {
+		$( '#popup' ).hide();
+	} );
 
 	//open apps -- inserted here --
 	const app = qlik.openApp('2b26dd49-cdcc-49ba-b4ee-31fbf436816d', config);
@@ -20,9 +25,15 @@ require(['js/qlik'], (qlik) => {
 	const app1 = qlik.openApp('17787584-b0e1-49f7-aceb-dd635fce80b4', config);
 
 	//get objects -- inserted here --
-	app1.getObject('QV06','xfvKMP');
-	app1.getObject('QV05','298bbd6d-f23d-4469-94a2-df243d680e0c');
-	app1.getObject('QV04','jTuCwkB');
-	app1.getObject('QV03','xa5e0f12c-38f5-4da9-8f3f-0e4566b28398');
-	app1.getObject('QV02','hRZaKk');
-	app.getObject('QV01','qQJqn');
+	app1.visualization.get('xfvKMP').then(function(vis){
+		vis.show("QV06");
+	app1.visualization.get('298bbd6d-f23d-4469-94a2-df243d680e0c').then(function(vis){
+		vis.show("QV05");
+	app1.visualization.get('jTuCwkB').then(function(vis){
+		vis.show("QV04");
+	app1.visualization.get('xa5e0f12c-38f5-4da9-8f3f-0e4566b28398').then(function(vis){
+		vis.show("QV03");
+	app1.visualization.get('hRZaKk').then(function(vis){
+		vis.show("QV02");
+	app.visualization.get('qQJqn').then(function(vis){
+		vis.show("QV01");
