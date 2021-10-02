1a5bf1f36bc3013733d19852150c606582f696ba
jonchw <90896455+jonchw@users.noreply.github.com>
2021-10-02 23:31:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,13 +27,19 @@ require(['js/qlik'], function (qlik) {
 	//get objects -- inserted here --
 	app1.visualization.get('xfvKMP').then(function(vis){
 		vis.show("QV06");
+	});
 	app1.visualization.get('298bbd6d-f23d-4469-94a2-df243d680e0c').then(function(vis){
 		vis.show("QV05");
+	});
 	app1.visualization.get('jTuCwkB').then(function(vis){
 		vis.show("QV04");
+	});
 	app1.visualization.get('xa5e0f12c-38f5-4da9-8f3f-0e4566b28398').then(function(vis){
 		vis.show("QV03");
+	});
 	app1.visualization.get('hRZaKk').then(function(vis){
 		vis.show("QV02");
+	});
 	app.visualization.get('qQJqn').then(function(vis){
 		vis.show("QV01");
+	});
