8f58de6eef4361fb0519706ae160af33dac5a388
jonchw <90896455+jonchw@users.noreply.github.com>
2021-11-05 14:28:45 +0800

    Update testtrial.js

--- testtrial.js
+++ testtrial.js
@@ -10,86 +10,11 @@ require.config({
   baseUrl: `https://${config.host}/resources`,
   webIntegrationId: config.webIntegrationId
 });
-require( ["js/qlik"], function ( qlik ) {
 
-	var control = false;
-	qlik.on( "error", function ( error ) {
-		$( '#popupText' ).append( error.message + "<br>" );
-		if ( !control ) {
-			control = true;
-			$( '#popup' ).delay( 1000 ).fadeIn( 1000 ).delay( 11000 ).fadeOut( 1000 );
-		}
-	} );
-
-	$( "#closePopup" ).click( function () {
-		$( '#popup' ).hide();
-	} );
-	if ( $( 'ul#qbmlist li' ).length === 0 ) {
-		$( '#qbmlist' ).append( "<li><a>No bookmarks available</a></li>" );
-	}
-	$( "body" ).css( "overflow: hidden;" );
-	function AppUi ( app ) {
-		var me = this;
-		this.app = app;
-		app.global.isPersonalMode( function ( reply ) {
-			me.isPersonalMode = reply.qReturn;
-		} );
-		app.getAppLayout( function ( layout ) {
-			$( "#title" ).html( layout.qTitle );
-			$( "#title" ).attr( "title", "Last reload:" + layout.qLastReloadTime.replace( /T/, ' ' ).replace( /Z/, ' ' ) );
-			//TODO: bootstrap tooltip ??
-		} );
-		app.getList( 'SelectionObject', function ( reply ) {
-			$( "[data-qcmd='back']" ).parent().toggleClass( 'disabled', reply.qSelectionObject.qBackCount < 1 );
-			$( "[data-qcmd='forward']" ).parent().toggleClass( 'disabled', reply.qSelectionObject.qForwardCount < 1 );
-		} );
-		app.getList( "BookmarkList", function ( reply ) {
-			var str = "";
-			reply.qBookmarkList.qItems.forEach( function ( value ) {
-				if ( value.qData.title ) {
-					str += '<li><a data-id="' + value.qInfo.qId + '">' + value.qData.title + '</a></li>';
-				}
-			} );
-			str += '<li><a data-cmd="create">Create</a></li>';
-			$( '#qbmlist' ).html( str ).find( 'a' ).on( 'click', function () {
-				var id = $( this ).data( 'id' );
-				if ( id ) {
-					app.bookmark.apply( id );
-				} else {
-					var cmd = $( this ).data( 'cmd' );
-					if ( cmd === "create" ) {
-						$( '#createBmModal' ).modal();
-					}
-				}
-			} );
-		} );
-		$( "[data-qcmd]" ).on( 'click', function () {
-			var $element = $( this );
-			switch ( $element.data( 'qcmd' ) ) {
-				//app level commands
-				case 'clearAll':
-					app.clearAll();
-					break;
-				case 'back':
-					app.back();
-					break;
-				case 'forward':
-					app.forward();
-					break;
-				case 'lockAll':
-					app.lockAll();
-					break;
-				case 'unlockAll':
-					app.unlockAll();
-					break;
-				case 'createBm':
-					var title = $( "#bmtitle" ).val(), desc = $( "#bmdesc" ).val();
-					app.bookmark.create( title, desc );
-					$( '#createBmModal' ).modal( 'hide' );
-					break;
-			}
-		} );
-	}
+require(["js/qlik"], qlik => {
+  qlik.on("error", error => console.error(error));
+  console.log("capabilit api:");
+	
 
 	//callbacks -- inserted here --
 	//open apps -- inserted here --
